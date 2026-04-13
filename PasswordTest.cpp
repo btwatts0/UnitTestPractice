17f3b8f06e31c4f50ce652c88a6b50cee6dfc488
btwatts0 <btwatts@csuchico.edu>
2026-04-13 11:48:33 -0700

    adds password tests + second function

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,9 +14,128 @@ class PasswordTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
+TEST(PasswordTest, two_repeats_three_total)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(2, my_password.count_leading_characters("aab"));
+}
+
+TEST(PasswordTest, one_repeat_three_total)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("baa"));
+}
+
+TEST(PasswordTest, five_repeats_six_total)
+{
+	Password my_password;
+	ASSERT_EQ(5, my_password.count_leading_characters("aaaaab"));
+}
+
+TEST(PasswordTest, character_returning_after_repeat_stops)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("aaba"));
+}
+
+TEST(PasswordTest, empty)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters(""));
+}
+
+TEST(PasswordTest, numbers)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("112"));
+}
+
+TEST(PasswordTest, case_change)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("Aab"));
+}
+
+TEST(PasswordTest, repeating_uppercase)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("AAab"));
+}
+
+TEST(PasswordTest, different_uppercase)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("AB"));
+}
+
+TEST(PasswordTest, special_characters)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("_AB"));
+}
+
+TEST(PasswordTest, space)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters(" "));
+}
+
+TEST(PasswordTest, one_lowercase)
+{
+	Password my_password;
+	EXPECT_FALSE(my_password.has_mixed_case("a"));
+}
+
+TEST(PasswordTest, one_uppercase)
+{
+	Password my_password;
+	EXPECT_FALSE(my_password.has_mixed_case("A"));
+}
+
+TEST(PasswordTest, lowercase_uppercase)
+{
+	Password my_password;
+	EXPECT_TRUE(my_password.has_mixed_case("aA"));
+}
+
+TEST(PasswordTest, uppercase_lowercase)
+{
+	Password my_password;
+	EXPECT_TRUE(my_password.has_mixed_case("Aa"));
+}
+
+TEST(PasswordTest, empty_mixed)
+{
+	Password my_password;
+	EXPECT_FALSE(my_password.has_mixed_case(""));
+}
+
+TEST(PasswordTest, multiple_same_case)
+{
+	Password my_password;
+	EXPECT_FALSE(my_password.has_mixed_case("aaaa"));
+}
+
+TEST(PasswordTest, multiple_same_case_then_mix)
+{
+	Password my_password;
+	EXPECT_TRUE(my_password.has_mixed_case("aaaaA"));
+}
+
+TEST(PasswordTest, lowercase_special)
+{
+	Password my_password;
+	EXPECT_FALSE(my_password.has_mixed_case("a%"));
+}
+
+TEST(PasswordTest, uppercase_special)
+{
+	Password my_password;
+	EXPECT_FALSE(my_password.has_mixed_case("A%"));
+}
+
+TEST(PasswordTest, special_then_mixed)
+{
+	Password my_password;
+	EXPECT_TRUE(my_password.has_mixed_case("^aA"));
 }
